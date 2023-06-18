23ccfa042732678e91916676db0d719c4baa6528
EC2 Default User <ec2-user@ip-172-31-23-21.ap-northeast-1.compute.internal>
2023-06-18 02:02:08 +0000

    [add]ヘッダーとフッターのレイアウト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,10 +16,25 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find(params[:id])
+    @book = Book.new
+    @book_find = Book.find(params[:id])
+    @user = current_user
   end
 
   def edit
+    @book_find = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
 # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about' => 'homes#about', as: 'about'
 
-  resources :users, only: [:show, :edit]
-  resources :books, only: [:new, :index, :show, :create]
+  resources :users, only: [:show, :edit, :index, :update]
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
