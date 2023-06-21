4e24601c8ab0e816ca669edd947a8d0e903cfc0c
EC2 Default User <ec2-user@ip-172-31-23-21.ap-northeast-1.compute.internal>
2023-06-21 13:01:06 +0000

    [Fix]プロフィール画像の表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,16 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
 
-  has_one_attached :image
+  has_one_attached :profile_image
 
   validates :name, uniqueness: true, length: {in: 2..200}
   validates :introduction, length: {maximum: 50}
 
   def get_profile_image(width, height)
-    unless image.attached?
+    unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
     end
-    image.variant(resize_to_limit: [width, height]).processed
+    profile_image.variant(resize_to_limit: [width, height]).processed
   end
 end
